54e833bfed53de56c799f515661b1df99de48cea
se-hoon Cho <951jth@delphicom.net>
2023-05-10 14:20:11 +0900

    페이지 정리

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import FfmpegComponent from './FfmpegComponent';
+import BrowserImageComp from './BrowserImageComp';
 // import MuxjsComponent from './MuxjsComponent';
 function App() {
+  const [page, setPage] = useState('FfmpegComponent');
+  const onPageMove = (e) => {
+    setPage(e?.target?.value);
+  };
   return (
     <>
-      <div className="App">
-        <h1>FfmpegComponent</h1>
-        <FfmpegComponent resolution="1920x1080" />
-        <FfmpegComponent resolution="1280x720" />
-        <FfmpegComponent resolution="854x480" />
-        <FfmpegComponent resolution="640x360" />
+      <div style={{ textAlign: 'center' }}>
+        <input
+          type="button"
+          value="FfmpegComponent"
+          onClick={onPageMove}
+        ></input>
+        <input
+          type="button"
+          value="MuxjsComponent"
+          onClick={onPageMove}
+        ></input>
       </div>
-      <div className="App">
-        <h1>MuxjsComponent</h1>
-        {/* <MuxjsComponent resolution="1920x1080" /> */}
-      </div>
+      {page === 'FfmpegComponent' && (
+        <>
+          <div className="App">
+            <h1>FfmpegComponent</h1>
+            <FfmpegComponent resolution="1920x1080" />
+            <FfmpegComponent resolution="1280x720" />
+            <FfmpegComponent resolution="854x480" />
+            <FfmpegComponent resolution="640x360" />
+          </div>
+        </>
+      )}
+      {page === 'MuxjsComponent' && (
+        <div className="App">
+          <h1>MuxjsComponent</h1>
+          {/* <MuxjsComponent resolution="1920x1080" /> */}
+        </div>
+      )}
+      {page === 'BrowserImageComp' && (
+        <div className="App">
+          <h1>BrowserImageComp</h1>
+          <BrowserImageComp />
+        </div>
+      )}
     </>
   );
 }
---
+++ src/BrowserImageComp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function BrowserImageComp() {
+  return (
+    <div>
+      <h3>BrowserImageComp</h3>
+    </div>
+  );
+}
